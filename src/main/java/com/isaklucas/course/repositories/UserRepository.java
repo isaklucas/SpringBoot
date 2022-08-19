404bba4baf545ec22915c7a3668b4ff343cd0ecf
isaklucas <isaklucas@gmail.com>
2022-08-19 15:27:39 -0300

    Jpa repository

---
+++ src/main/java/com/isaklucas/course/repositories/UserRepository.java
@@ -0,0 +1,17 @@
+package com.isaklucas.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.isaklucas.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	
+	
+	
+	
+	
+	
+	
+
+}

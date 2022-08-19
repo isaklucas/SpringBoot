21dc1a9dfdbb5096177ee4a9e278014d9184110f
isaklucas <isaklucas@gmail.com>
2022-08-19 15:15:38 -0300

    H2 database , JPA

--- src/main/java/com/isaklucas/course/entities/User.java
+++ src/main/java/com/isaklucas/course/entities/User.java
@@ -3,12 +3,24 @@ package com.isaklucas.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "users")
 public class User implements Serializable {
 	
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
 	private String email;
 	private String phone;

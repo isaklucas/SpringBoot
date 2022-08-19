a866f8db0b3b990bd83f37f219441ede0013cdc1
isaklucas <isaklucas@gmail.com>
2022-08-19 15:42:59 -0300

    Service Layer

--- src/main/java/com/isaklucas/course/resources/UserResource.java
+++ src/main/java/com/isaklucas/course/resources/UserResource.java
@@ -1,24 +1,38 @@
 package com.isaklucas.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.isaklucas.course.entities.User;
+import com.isaklucas.course.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
+	public ResponseEntity<List<User>> findAll(){
+		
+		List<User> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+
+				
+	}
 	
-		User u = new User(1L, "Maria", "Maria@gmail.com", "999999", "99999999");
-		return ResponseEntity.ok().body(u);
-				
-				
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 

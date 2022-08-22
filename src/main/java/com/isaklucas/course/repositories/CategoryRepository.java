2ba12792b0f52cbe8bba97f528e110df21c785f3
isaklucas <isaklucas@gmail.com>
2022-08-22 17:23:25 -0300

    Category

---
+++ src/main/java/com/isaklucas/course/repositories/CategoryRepository.java
@@ -0,0 +1,17 @@
+package com.isaklucas.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.isaklucas.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+	
+	
+	
+	
+	
+	
+	
+
+}

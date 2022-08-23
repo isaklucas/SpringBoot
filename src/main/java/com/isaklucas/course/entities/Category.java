b86e3204adcd2694691ef8626a8ff153eb153540
isaklucas <isaklucas@gmail.com>
2022-08-23 09:13:52 -0300

    ManyToMany

--- src/main/java/com/isaklucas/course/entities/Category.java
+++ src/main/java/com/isaklucas/course/entities/Category.java
@@ -9,27 +9,28 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_category")
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	private String name;
-	
-	
-	@Transient
+
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
-	
+
 	public Category() {
-		
+
 	}
 
 	public Category(Long id, String name) {
@@ -58,7 +59,7 @@ public class Category implements Serializable {
 	public Set<Product> getProducts() {
 		return products;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -76,7 +77,4 @@ public class Category implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
-
-	
-	
 }

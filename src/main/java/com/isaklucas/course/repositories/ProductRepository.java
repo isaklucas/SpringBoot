73e279d4d80fc31739bbaf3444859432273e0202
isaklucas <isaklucas@gmail.com>
2022-08-23 08:54:45 -0300

    Products

---
+++ src/main/java/com/isaklucas/course/repositories/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.isaklucas.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.isaklucas.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
--- src/main/java/com/isaklucas/course/resources/ProductResource.java
+++ src/main/java/com/isaklucas/course/resources/ProductResource.java
@@ -9,20 +9,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.isaklucas.course.entities.Category;
-import com.isaklucas.course.services.CategoryService;
+import com.isaklucas.course.entities.Product;
+import com.isaklucas.course.services.ProductService;
 
 @RestController
-@RequestMapping(value="/categories")
-public class CategoryResource {
+@RequestMapping(value="/products")
+public class ProductResource {
 	
 	@Autowired
-	private CategoryService service;
+	private ProductService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
+	public ResponseEntity<List<Product>> findAll(){
 		
-		List<Category> list = service.findAll();
+		List<Product> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
 
@@ -30,8 +30,8 @@ public class CategoryResource {
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Category> findById(@PathVariable Long id) {
-		Category obj = service.findById(id);
+	public ResponseEntity<Product> findById(@PathVariable Long id) {
+		Product obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	

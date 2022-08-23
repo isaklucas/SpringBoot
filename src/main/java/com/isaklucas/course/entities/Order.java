fa9209c198050cbba06653f86b963c7a575c67be
isaklucas <isaklucas@gmail.com>
2022-08-23 09:49:50 -0300

    OrderItem

--- src/main/java/com/isaklucas/course/entities/Order.java
+++ src/main/java/com/isaklucas/course/entities/Order.java
@@ -2,7 +2,9 @@ package com.isaklucas.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -22,7 +25,7 @@ import com.isaklucas.course.entities.enums.OrderStatus;
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
@@ -34,6 +37,9 @@ import com.isaklucas.course.entities.enums.OrderStatus;
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Order() {
 		
@@ -49,6 +55,9 @@ import com.isaklucas.course.entities.enums.OrderStatus;
 	
 	}
 
+	public Set<OrderItem> getItems(){
+		return items;
+	}
 
 	public Long getId() {
 		return id;
---
+++ src/main/java/com/isaklucas/course/repositories/OrderItemRepository.java
@@ -0,0 +1,17 @@
+package com.isaklucas.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.isaklucas.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
